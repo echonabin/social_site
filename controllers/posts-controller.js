aa42fab386eacdc8c141f5111000ab08ea189307
echonabin <echonabin@gmail.com>
2020-10-03 08:18:55 +0545

    Get all post delete post and get single post added

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,3 +1,79 @@
-module.exports.get_post = (req, res, next) => {
-  res.status(200).json({ message: "success with post" });
+const Post = require("../models/Post");
+const User = require("../models/User");
+const router = require("../routes/api/auth-route");
+const auth = require("../middleware/auth");
+
+//@route       POST api/posts
+//desc         Create a post
+//@access      Private
+module.exports.post_post = async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  try {
+    const newPost = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    };
+    const Posts = new Post(newPost);
+    const posts = await Posts.save();
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+//@route       GET api/posts
+//desc         get all posts from a user
+//@access      Private
+
+module.exports.get_all_posts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+//@route       GET api/posts/:id
+//desc         get single posts from post id
+//@access      Private
+
+module.exports.get_single_post = async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const post = await Post.findById(postId);
+    res.json(post);
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "No post found." });
+    }
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+//@route       DELETE api/posts/:id
+//desc         delete single post provided with post id
+//@access      Private
+
+module.exports.delete_post = async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (post.user.toString() === user._id.toString())
+      await Post.findByIdAndRemove(postId);
+    else {
+      res.status(400).json("This post dosen't belog to you");
+    }
+    res.json("Post Deleted Succesfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
--- routes/api/posts-route.js
+++ routes/api/posts-route.js
@@ -1,7 +1,31 @@
 const { Router } = require("express");
 const postController = require("../../controllers/posts-controller");
+const auth = require("../../middleware/auth");
+const { validatePost } = require("../../middleware/validators/post-validator");
 const router = Router();
 
-router.get("/", postController.get_post);
+//@route       POST api/posts
+//desc         Create a post
+//@access      Private
+
+router.post("/", auth, validatePost, postController.post_post);
+
+//@route       GET api/posts
+//desc         get all posts from a user
+//@access      Private
+
+router.get("/", auth, postController.get_all_posts);
+
+//@route       GET api/posts/:id
+//desc         get single posts from post id
+//@access      Private
+
+router.get("/:id", auth, postController.get_single_post);
+
+//@route       DELETE api/posts/:id
+//desc         delete single post provided with post id
+//@access      Private
+
+router.delete("/:id", auth, postController.delete_post);
 
 module.exports = router;
