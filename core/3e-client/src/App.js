c834774a7dc5f37bcf72872ed74600629ccbc796
echonabin <echonabin@gmail.com>
2020-10-05 00:23:26 +0545

    nav bar and landing page added

--- core/3e-client/src/App.js
+++ core/3e-client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
 const App = () => {
   return (
     <>
+      <Navbar />
+      <Landing />
       <h1>Hello</h1>
     </>
   );
