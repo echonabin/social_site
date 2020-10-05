d81eafab1ed157eec4b2aaed72d1ba726b405a45
echonabin <echonabin@gmail.com>
2020-10-05 15:32:54 +0545

    redux store added and login page added

--- core/3e-client/src/App.js
+++ core/3e-client/src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+//Redux
+import store from "./store";
+
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-          </Switch>
-        </section>
-      </>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
 };
 
