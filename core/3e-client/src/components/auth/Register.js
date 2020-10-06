c5dfc12e01176267a314738b0f91ac5e4e49a700
echonabin <echonabin@gmail.com>
2020-10-06 13:33:54 +0545

    Extra errors added

--- core/3e-client/src/components/auth/Register.js
+++ core/3e-client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,7 +22,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Password don't match", "danger");
     } else {
-      console.log(formData);
+      register({ name, email, password });
     }
   };
   return (
@@ -63,7 +64,7 @@ const Register = ({ setAlert }) => {
             type='password'
             placeholder='Password'
             name='password'
-            minLength='6'
+            minLength='8'
             value={password}
             onChange={(e) => onChange(e)}
           />
@@ -73,7 +74,7 @@ const Register = ({ setAlert }) => {
             type='password'
             placeholder='Confirm Password'
             name='password2'
-            minLength='6'
+            minLength='8'
             value={password2}
             onChange={(e) => onChange(e)}
           />
@@ -89,6 +90,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
---
+++ core/3e-client/src/reducers/auth.js
@@ -0,0 +1,22 @@
+import { REGISTER_FAIL, REGISTER_SUCCESS } from "../types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return { ...state, ...payload, isAuthenticated: true, loading: false };
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return { ...state, token: null, isAuthenticated: false, loading: false };
+
+    default:
+      return state;
+  }
+};
