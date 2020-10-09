92ff477b7297639e92813eb89ff0b69544619706
echonabin <echonabin@gmail.com>
2020-10-09 15:32:32 +0545

    Display profile completed

--- core/3e-client/src/components/profiles/Profiles.js
+++ core/3e-client/src/components/profiles/Profiles.js
@@ -1,19 +1,40 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
+import ProfileItem from "./ProfileItem";
 
 //Redux
 import { connect } from "react-redux";
 import { getProfiles } from "../../actions/profile";
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
-  useEffect[
-    (() => {
-      getProfiles();
-    },
-    [])
-  ];
-  return <div></div>;
+  useEffect(() => {
+    getProfiles();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop'></i>
+            {"  "} Browse and connect with Developers
+          </p>
+          <div className='profiles'>
+            {profiles.length ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4> No Profiles Found </h4>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 Profiles.propTypes = {
