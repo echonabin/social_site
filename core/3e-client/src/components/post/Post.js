8f47d0e96a60b682b88df06a0bf3776f1783b867
echonabin <echonabin@gmail.com>
2020-10-12 19:17:39 +0545

    add comment and delete comment feature added

--- core/3e-client/src/components/post/Post.js
+++ core/3e-client/src/components/post/Post.js
@@ -7,6 +7,7 @@ import { connect } from "react-redux";
 import { getPost } from "../../actions/post";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => getPost(match.params.id), [getPost]);
@@ -19,6 +20,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem showActions={false} post={post} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </>
   );
 };
